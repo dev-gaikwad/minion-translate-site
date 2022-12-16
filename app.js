517a539b1df917381d3cb3d75ae775157cd5b1cd
dev-gaikwad <dev-gaikwad@users.noreply.github.com>
2022-12-16 19:54:05 +0530

    feat: add error handling

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output-box")
-
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output-box');
 
 // var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var url = "https://api.funtranslations.com/translate/minion.json"
+var url = 'https://api.funtranslations.com/translate/minion.json';
 
-function constructURL(text){
-    var newURL = url + "?text=" + text ;
-    console.log(newURL);
-    return newURL ;
+function constructURL(text) {
+  var newURL = url + '?text=' + text;
+  console.log(newURL);
+  return newURL;
 }
 
-function clickHandler(){
-
-    var inputText = txtInput.value ;
-    fetch(constructURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translated ;
-        outputDiv.innerText = translatedText ;
+function clickHandler() {
+  var inputText = txtInput.value;
+  fetch(constructURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
     })
+    .catch((error) =>
+      alert(
+        `ERROR -\nProbably the translation limit for this hour has ended \nTry again after sometime`,
+        error
+      )
+    );
 }
 
-btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener('click', clickHandler);
